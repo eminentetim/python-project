08dfbdbd8a857f8b69189b30198ee199c5353827
eminentetim <etimeminent@gmail.com>
2023-10-09 13:13:50 +0100

    creating a function that check age and allocate class for student

---
+++ Python/self-practise/birthday-checker.py
@@ -0,0 +1,10 @@
+age = eval(input("Enter age: "))
+
+if (age >= 1) and (age <= 18):
+	print("Important Bithday")
+elif (age == 21) or (age == 50):
+	print("Important Birthday")
+elif not(age < 65):
+	print("Important birthday")
+else:
+	print("sorry Not Important ")
